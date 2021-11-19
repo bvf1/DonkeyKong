c73177abe1bd6b63f3486ef252997349ddef7dc0
OskarAgnarsson <osa16@hi.is>
2021-11-19 17:49:21 +0000

    lagaði að barrel dóu ekki á oil

--- Barrel.js
+++ Barrel.js
@@ -130,11 +130,6 @@ Barrel.prototype._draw = function (ctx) {
 }
 
 
-
-Barrel.prototype._moveBarrelDown = function(brick, barrel) {
-     barrel._cy = brick.theBrick.getPos().posX;
-}
-
 var BARREL_SPEED = 2.5;
 var GRAVITY = 0.12;
 
@@ -146,10 +141,10 @@ Barrel.prototype.normalMovement = function (du) {
 
     var aveVelY = (oldVelY + this.velY) / 2;
     //Need this so barrels don't fall off world in some instances
-    if (this.fallingTime > du*6 &&!this.specialBarrel) {
+    if (this.fallingTime > 4 && !this.specialBarrel) {
         this.cx += this.direction*BARREL_SPEED/4*du;
     }
-    if (!this.laddering && this.fallingTime < du*6) {
+    if (!this.laddering && this.fallingTime < 4) {
         this.cx += this.direction*BARREL_SPEED*du;
     }
     this.cy += aveVelY * 0.005*SECS_TO_NOMINALS;
@@ -193,15 +188,15 @@ Barrel.prototype.update = function (du) {
             if (collision[0].tag === "Brick") {
 
                 //Stop laddering after a certain period so barrel doesn't fall through the world
-                if (this.ladderTime > du*24 && this.specialBarrel === false) {
+                if (this.ladderTime > 12 && !this.specialBarrel) {
                     this.laddering = false;
                 }
                 //Don't clip through the floor
-                if (this.cy + this.getRadius() > collision[0].getPos().posY - collision[0].getSize().height/2 && !this.laddering && this.specialBarrel) {
+                if (this.cy + this.getRadius() > collision[0].getPos().posY - collision[0].getSize().height/2 && !this.laddering) {
                     this.cy = collision[0].getPos().posY - collision[0].getSize().height/2 - this.getRadius();
                 }
                 //Turn around after falling
-                if (this.falling && this.fallingTime > du*24 && !this.starting || this.ladderTime > du*24 && !this.specialBarrel) {
+                if (this.falling && this.fallingTime > 12 && !this.starting || this.ladderTime > 12 && !this.specialBarrel) {
                     this.direction *= -1;
                     this.ladderTime = 0;
 
